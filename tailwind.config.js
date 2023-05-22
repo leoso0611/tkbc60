3084a6c85bd55b9a23d6c550e29bbad693df73bf
Leo So <leo.so@cydeas.com>
2023-05-22 16:16:17 +0000

    tailwind added custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#f2f0e4",
+        "footer": "#eae6d7",
+        "text": "#4162a2"
+      }
+    },
   },
   plugins: [],
 }
