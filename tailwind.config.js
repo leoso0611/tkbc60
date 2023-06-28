0138ff71ebc4dfb05596a591182e06f27547185a
Leo So <leosohc@gmail.com>
2023-06-28 21:50:03 +0800

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'noto': ['"Noto Sans TC"', 'sans-serif']
+      },
       colors: {
         "primary": "#1B252F",
         "secondary": "#F2F0E4",
+        "primaryBackground": "#FAF8F1",
         "mobilemenutab": "#20438E",
         "copyrighttext": "#174092"
       }
