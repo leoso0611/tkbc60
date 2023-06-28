450fc9c57901e584e1692cc8b8d23f93e2d63d67
Leo So <leosohc@gmail.com>
2023-06-28 21:50:26 +0800

    added title and page layout

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,5 +1,13 @@
+import Pagetitle from "components/pagetitle";
+
 function List() {
-  return <div>List</div>;
+  return (
+    <div className="bg-primaryBackground">
+      <div className="max-w-screen-xl mx-auto px-6 py-12 md:px-4 md:py-10">
+        <Pagetitle title="作品集" />
+      </div>
+    </div>
+  );
 }
 
 export default List;
