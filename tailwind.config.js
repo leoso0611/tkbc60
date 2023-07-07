bd8f0a78c85838cc20c4ae31606f88d347e7e93b
alexlam184 <alexwork0211@gmail.com>
2023-07-08 00:32:38 +0800

    fix :

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'noto': ['"Noto Sans TC"', 'sans-serif']
+        noto: ['"Noto Sans TC"', "sans-serif"],
       },
       colors: {
         primary: "#1B252F",
         secondary: "#F2F0E4",
         primaryBackground: "#FAF8F1",
         mobilemenutab: "#20438E",
-        copyrighttext: "#174092"
+        copyrighttext: "#174092",
         wire: "#918D7B",
         photo: "#061c47",
         diy: "#F2434C",
