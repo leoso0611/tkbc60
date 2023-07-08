6d16d54605a1aa16c9c3d238c37c9775b6d7b4d0
Leo So <leosohc@gmail.com>
2023-07-09 01:05:40 +0800

    fixed css position problem

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -10,27 +10,25 @@ function Showcase({ item, setOpen, typeTitle }) {
     setOpen(false);
   };
   return (
-    <div className="bg-secondary w-full h-full md:p-8 p-4 overflow-scroll">
-      <div className="w-full flex justify-end">
-        <button
+    <div className="bg-secondary w-full h-full md:p-8 p-4 no-scrollbar overflow-scroll relative">
+      <button
           onClick={handleModalClose}
-          className="hover:bg-gray-300 rounded-full w-8 h-8 flex justify-center items-center"
+          className="hover:bg-gray-300 rounded-full w-8 h-8 flex justify-center items-center absolute right-8"
         >
           <img src={Close} alt="close" className="w-6 h-6" />
         </button>
-      </div>
-      <div className="flex md:flex-row flex-col h-[90%] md:mt-0 mt-3">
-        <div className="md:flex-[2]">
+      <div className="flex md:flex-row flex-col h-full md:mt-0 mt-3">
+        <div className="md:flex-[2] md:flex md:items-center">
           {item.type !== "sound" ? (
             <img
               src={source}
               alt="show"
-              className="h-full object-contain object-left-top"
+              className="h-full object-contain"
             />
           ) : null}
         </div>
 
-        <div className="md:flex-1 md:px-6 md:py-10 px-0 py-0 md:mt-0 mt-6">
+        <div className="md:flex-1 md:px-6 md:py-10 px-0 py-0 md:mt-0 mt-6 md:flex md:flex-col md:justify-start md:justify-center">
           <div className="flex items-center">
             <img src={Clip} alt="clip" className="w-10"/> {/* TODO: switch type => different clip color */}
             {returnTitle(type)}
