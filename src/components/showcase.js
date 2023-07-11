80a90eb96e08f185b11ce9b1e52c4f45ca55e249
Leo So <leosohc@gmail.com>
2023-07-11 15:26:31 +0800

    change the close button

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -13,25 +13,42 @@ function Showcase({ item, setOpen, open }) {
   return (
     <div className="bg-secondary w-full h-full md:p-8 p-4 no-scrollbar overflow-scroll relative">
       <button
-          onClick={handleModalClose}
-          className="hover:bg-gray-300 rounded-full w-8 h-8 flex justify-center items-center absolute md:right-8 right-4"
+        onClick={handleModalClose}
+        className="hover:bg-gray-300 rounded-full w-8 h-8 flex justify-center items-center absolute md:right-8 right-4"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="23"
+          height="23"
+          viewBox="0 0 23 23"
+          fill="none"
         >
-          <img src={Close} alt="close" className="w-6 h-6" />
-        </button>
+          <path
+            d="M3 3L20 20M3 20L20 3"
+            stroke="#1B252F"
+            stroke-width="5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
       <div className="flex md:flex-row flex-col h-full md:mt-0 mt-10">
         <div className="md:flex-[2] md:flex md:items-center">
           {item.type !== "sound" ? (
             <img
               src={source}
               alt="show"
-              className="md:h-full object-contain"
+              className="md:h-full object-contain object-center"
             />
-          ) : <Player item={item} open={open}/>}
+          ) : (
+            <Player item={item} open={open} />
+          )}
         </div>
 
         <div className="md:flex-1 md:px-6 md:py-10 px-0 pt-0 pb-8 md:mt-0 mt-6 md:flex md:flex-col md:justify-start md:justify-center">
           <div className="flex items-center">
-            <img src={Clip} alt="clip" className="w-10"/> {/* TODO: switch type => different clip color */}
+            <img src={Clip} alt="clip" className="w-10" />{" "}
+            {/* TODO: switch type => different clip color */}
             {returnTitle(type)}
           </div>
           <div className="mt-4 font-bold text-2xl">{author_name}</div>
@@ -52,7 +69,7 @@ function Showcase({ item, setOpen, open }) {
 Showcase.propTypes = {
   item: PropTypes.object,
   setOpen: PropTypes.func,
-  open: PropTypes.bool
+  open: PropTypes.bool,
 };
 
 export default Showcase;
