2d6ac33029c175fa8d788add7f394ba54cc7c0a0
Leo So <leosohc@gmail.com>
2023-07-11 22:34:48 +0800

    fixed player bugs

--- src/components/player.js
+++ src/components/player.js
@@ -12,7 +12,7 @@ function Player({ item, open }) {
     sec: "",
   }); // current position of the audio in minutes and seconds
 
-  const [seconds, setSeconds] = useState();
+  const [seconds, setSeconds] = useState(0);
   const [play, { pause, stop, duration, sound }] = useSound(source);
 
   const sec = duration / 1000;
@@ -63,12 +63,12 @@ function Player({ item, open }) {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-10 h-10 fill-primary"
+                className="w-10 h-10 fill-primary"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </button>
@@ -78,12 +78,12 @@ function Player({ item, open }) {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="w-10 h-10 fill-primary"
+                className="w-10 h-10 fill-primary"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </button>
@@ -117,7 +117,7 @@ function Player({ item, open }) {
 
 Player.propTypes = {
   item: PropTypes.object,
-  open: PropTypes.object,
+  open: PropTypes.bool,
 };
 
 export default Player;
